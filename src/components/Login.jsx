43ce21d6aac2908155decf04436011775bc649c8
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-08 20:24:43 +0600

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center ">
+                    <h1 className="text-5xl font-bold">Please Login !</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <label >
+                            <Link to='/register' className="label-text-alt link link-hover">New to auth-master please register</Link>
+                        </label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR6SoDAyuE7XZKma7nhhZAWAC8D0nKaEs",
+  authDomain: "auth-firebase-context-ta-fc394.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-fc394",
+  storageBucket: "auth-firebase-context-ta-fc394.appspot.com",
+  messagingSenderId: "279893612533",
+  appId: "1:279893612533:web:bbada5204f8f3b743732bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
